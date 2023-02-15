6b31dc7e3a1504c9943e0fbb98c5b85d9c171216
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-02-14 20:24:38 -0500

    Agregar cambios

---
+++ 2. SCRIPTS/1_Limpieza_Datos_Training.R
@@ -0,0 +1,27 @@
+#Problmem_Set_2
+# Limpieza de datos Trainign
+#Carlos Vergara, Alexandra Rizo, Danna Bolaños, Héctor Tacumán
+_______________________________________________________________
+
+#######Preparación del espacio########
+
+## Se llaman los paquetes para su uso en el Script:
+install.packages("pacman")
+require(pacman)
+p_load(tidyverse,rvest,writexl,rio,skimr,pastecs,PerformanceAnalytics,naniar,gtsummary)
+
+## Se llaman las bases de datos de Training: 
+rm(list=ls())
+setwd("~/OneDrive - Universidad de los Andes/2023/2023-1/BIG DATA/TALLERES GRUPALES/TALLER No. 2/Problem_Set_2-_Predicting_Poverty/3. STORE")
+
+df_training_hogares <- import("train_hogares.csv")
+
+df_training_personas <- import("train_personas.csv")
+
+colnames(df_training_hogares)
+colnames (df_training_personas)
+
+summary(df_training_hogares)
+summary(df_training_personas)
+
+#######Modificación de las bases de datos########
