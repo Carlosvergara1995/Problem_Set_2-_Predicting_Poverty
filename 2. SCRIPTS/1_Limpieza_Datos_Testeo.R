b2ab6cc8321cad02760e28ed260b452d4b0ad371
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-02-16 21:39:56 -0500

    Se crea el script para la limpieza de base de datos de testeo.

---
+++ 2. SCRIPTS/1_Limpieza_Datos_Testeo.R
@@ -0,0 +1,14 @@
+#Problmem_Set_2
+# Limpieza de datos testeo y unificación de bases
+#Carlos Vergara, Alexandra Rizo, Danna Bolaños, Héctor Tacumán
+_______________________________________________________________
+
+####### Preparación del espacio ########
+## Se llaman los paquetes para su uso en el Script:
+install.packages("pacman")
+require(pacman)
+p_load(tidyverse,rvest,writexl,rio,skimr,pastecs,PerformanceAnalytics,naniar,gtsummary)
+
+## Se llaman las bases de datos de Training: 
+rm(list=ls())
+setwd("~/OneDrive - Universidad de los Andes/2023/2023-1/BIG DATA/TALLERES GRUPALES/TALLER No. 2/Problem_Set_2-_Predicting_Poverty/3. STORE")
