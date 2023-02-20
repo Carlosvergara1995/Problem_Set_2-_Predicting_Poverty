b9403612894ec035363d76a2746a294641062f5d
dannabn12 <dannab.n12@gmail.com>
2023-02-19 21:00:55 -0500

    Update Analisis nas.R

--- 2. SCRIPTS/Analisis nas.R
+++ 2. SCRIPTS/Analisis nas.R
@@ -1,6 +1,6 @@
 #Cargamos nuestras librearias 
 require(pacman)
-p_load(tidyverse, rio)
+p_load(tidyverse, rio,skimr,dplyr)
 
 #Cargamos la bases ensambladas finales de datos
 setwd('~/Desktop/git hut repositorios/Problem_Set_2/3. STORE')
@@ -12,14 +12,87 @@ training_1<- training
 test <- import("df_test_hogares_VF.rds")
 test_1 <- test
 
-############ Analisis na
+#analisis de estructura de nuestra base
+skim(training)
+summary(training)
 
-#Analisis de na por variable
-colSums(is.na(data))
+#analisis para construccion de la variable dependiente
+table(training$Pobre, useNA = "always")
+table(training$Indigente, useNA = "always")
 
-#Calculamos el porcentaje de los datos diferentes de NA
-sum(data$y_ingLab_m_ha > 0 & !is.na(data$y_ingLab_m_ha) )/length(data$y_ingLab_m_ha)
-# se infiere que el 43% de la base presenta datos 
+# Analisis na para training####
+attach(training)
+# Analisis de na por variable
+na_count<-colSums(is.na(training))
+na_cols <- na_count[na_count > 0]
+na_cols
 
-#eliminamos todas las filas con un valor faltante en la columna de nuestra valiable dependiente (y_ingLab_m_ha)
-df <- data[!is.na(data$y_ingLab_m_ha), ] %>% as.data.frame()
+#encontramos que las variables que contienne na son P5100, P5130,  P5140, cuota_amortizacion, arriendo, edu_promedio, horas_trabajadas_promedio,
+# las cuales corresponden a ¿Cuánto pagan mensualmente por cuota de amortización?; Si tuviera que pagar arriendo por esta vivienda,
+# ¿cuánto estima que tendría que pagar mensualmente?; ¿ Cuánto pagan mensualmente por arriendo ?; cuota de amortización; 
+#analizamos el porcentaje de nas presente en cada variable 
+
+# Calcular el porcentaje de valores NA por columna
+porcentaje_nulos_por_columna <- na_count / nrow(training) * 100
+
+# Seleccionar solo las variables con NA
+variables_con_na <- names(porcentaje_nulos_por_columna[porcentaje_nulos_por_columna > 0])
+#resultados
+print(paste("Variables con NA:", paste(variables_con_na, collapse = ", ")))
+print(paste("Porcentaje de NA por variable:"))
+print(porcentaje_nulos_por_columna[porcentaje_nulos_por_columna > 0])
+
+#encontramos que la variable P5100 y cuota_amortización se ecnuentran casi completanmente vacias con un por centaje de na del 96%
+# por otro lado las demas variables con na tambien presentan valores relativamente grandes de nas eceptuando a horas_trabajadas_promedio y edu_promedio
+# dados los resultados anteriores y las demas variables presentes en la base procederemos a realizar nuestro analisis sin ellas 
+# reemplazandolas con otras varibales, menos en el caso de edu_promedio que solo presenta 1 na el cual sera reemplazado por su promedio
+
+#codificamos nuestra variable edu_promedio dado que solo tiene un na lo reemplazaresmos con el promedio y eliminamos las variabbles con altos porcentajes de nas
+#calculamos el promedio de la variable
+media_edu_promedio <- mean(training$edu_promedio, na.rm = TRUE)
+variables_eliminar <- c('P5100', 'P5130',  'P5140', 'cuota_amortizacion', 'arriendo', 'horas_trabajadas_promedio')
+
+training<- training %>%
+  mutate( edu_promedio = ifelse(is.na(edu_promedio), media_edu_promedio, edu_promedio))%>%
+  select(-variables_eliminar)
+
+#se realiza el mismo procedimiento en test
+attach(test)
+na_count_test<-colSums(is.na(test))
+na_cols_test <- na_count_test[na_count_test > 0]
+na_cols_test
+#encontramos que las mismas variables presentan nas aunque con diferentes cifras a los de training
+
+# Calcular el porcentaje de valores NA por columna
+porcentaje_nulos_por_columna_t <- na_count_test / nrow(test) * 100
+
+# Seleccionar solo las variables con NA
+variables_con_na_t <- names(porcentaje_nulos_por_columna_t[porcentaje_nulos_por_columna_t > 0])
+#resultados
+print(paste("Variables con NA:", paste(variables_con_na_t, collapse = ", ")))
+print(paste("Porcentaje de NA por variable:"))
+print(porcentaje_nulos_por_columna_t[porcentaje_nulos_por_columna_t > 0])
+
+#codificamos nuestra variable edu_promedio dado que solo tiene un na lo reemplazaresmos con el promedio y eliminamos las variabbles con altos porcentajes de nas
+#calculamos el promedio de la variable
+
+media_edu_promediot <- mean(test$edu_promedio, na.rm = TRUE)
+variables_eliminart <- c('P5100', 'P5130',  'P5140', 'cuota_amortizacion', 'arriendo', 'horas_trabajadas_promedio')
+test<- test %>%
+  mutate( edu_promedio = ifelse(is.na(edu_promedio), media_edu_promediot, edu_promedio))%>%
+  select(-variables_eliminar)
+
+#creamos nuestra variable dependiente para predecir la probleza en los hogares ####
+
+training$pobreza<- ifelse(training$Pobre == 1 | training$Indigente == 1, 1, 0)
+
+skim(training$pobreza)
+
+pobrezatabla <- table(training$pobreza, useNA = "always")
+pobrezatabla
+
+#encontramos que de las  164960 observaciones 33024 hogares se encuentran en estado de pobreza o indigencia 
+
+saveRDS(test, "test_sinna.rds")
+saveRDS(training, "training_sinna.rds")
+
