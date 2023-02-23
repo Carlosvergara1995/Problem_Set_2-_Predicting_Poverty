5c312616561d060a6f13b7a681318631848f2b99
dannabn12 <dannab.n12@gmail.com>
2023-02-23 10:38:00 -0500

    Update 0_modificacion_basesdatos_formatords.R

--- 2. SCRIPTS/0_modificacion_basesdatos_formatords.R
+++ 2. SCRIPTS/0_modificacion_basesdatos_formatords.R
@@ -1,18 +1,17 @@
 #Problmem_Set_2
 # Transformación de bases de datos a formato Rds.
 #Carlos Vergara, Alexandra Rizo, Danna Bolaños, Héctor Tacumán
-_______________________________________________________________
+
 
 #######Preparación del espacio########
-
+rm(list=ls())
 ## Se llaman los paquetes para su uso en el Script:
 install.packages("pacman")
 require(pacman)
 p_load(tidyverse,rvest,writexl,rio,skimr,pastecs,PerformanceAnalytics,naniar,gtsummary)
 
 ## Se llaman las bases de datos: 
-rm(list=ls())
-setwd("~/OneDrive - Universidad de los Andes/uniandes-bdml-20231-ps2 2")
+setwd("~/OneDrive - Universidad de los Andes/uniandes-bdml-20231-ps2 2") #se cargan los archivos de la carpeta store directamente
 
 df_training_hogares <- import("train_hogares.csv") 
 df_training_personas <- import("train_personas.csv")
