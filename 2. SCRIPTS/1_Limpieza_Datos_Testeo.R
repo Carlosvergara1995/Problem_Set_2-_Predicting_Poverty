dd2da42f5c657f0a6579e95373255886165f5f19
dannabn12 <dannab.n12@gmail.com>
2023-02-23 17:56:21 -0500

    Update 1_Limpieza_Datos_Testeo.R

--- 2. SCRIPTS/1_Limpieza_Datos_Testeo.R
+++ 2. SCRIPTS/1_Limpieza_Datos_Testeo.R
@@ -10,7 +10,6 @@ require(pacman)
 p_load(tidyverse,rvest,writexl,rio,skimr,pastecs,PerformanceAnalytics,naniar,gtsummary)
 
 ## Se llaman las bases de datos de Training: 
-rm(list=ls())
 setwd("~/OneDrive - Universidad de los Andes/2023/2023-1/BIG DATA/TALLERES GRUPALES/TALLER No. 2/Problem_Set_2-_Predicting_Poverty/3. STORE")
 
 df_test_hogares <- import("df_test_hogares.rds") 
