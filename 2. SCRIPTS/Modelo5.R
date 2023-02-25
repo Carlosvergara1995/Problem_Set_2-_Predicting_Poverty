672007388db9d74fbaaf66a3e7d0b34752c2556b
dannabn12 <dannab.n12@gmail.com>
2023-02-24 21:09:02 -0500

    Update Modelo5.R

--- 2. SCRIPTS/Modelo5.R
+++ 2. SCRIPTS/Modelo5.R
@@ -6,7 +6,7 @@ p_load(tidyverse, rio,skimr,dplyr, caret, glmnet,smotefamily,ROSE)
 set.seed(123) 
 
 #logit con lasticnet REVISAR NOMBRWE
-modelo5<- train(x = select(db_train, -Pobre)
+modelo5<- train(x = select(db_train, -Pobre),
                 y= as.factor(db_train$Pobre),
                 preProcess=NULL,
                 method = "glmnet")
