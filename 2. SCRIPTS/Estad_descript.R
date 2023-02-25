6ca6fc801b7d4868cf059d3702823a97872e1cb1
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-02-25 18:13:42 -0500

    Update Estad_descript.R

--- 2. SCRIPTS/Estad_descript.R
+++ 2. SCRIPTS/Estad_descript.R
@@ -22,7 +22,7 @@ colnames(dvf_train)
 
 ## Se seleccionan variables de interes:
 
-df_1 <- dvf_train %>% select(c("Nper","Pobre.1","Nro_cuartos", "Nro_personas_cuartos","arriendo","Nro_mujeres","edad_promedio","jefe_hogar_mujer","Nro_hijos","Nro_personas_trabajo_formal","edu_promedio","Nro_personas_subsidio_familiar","horas_trabajadas_promedio","Nro_personas_empleo_propio","Nro_personas_segundo_trabajo","Nro_personas_arriendos","Nro_personas_pensiones","Nro_personas_pension_alimenticia","Nro_personas_otros_ingresos","Nro_personas_otros_ingresos_pais","Nro_personas_otros_ingresos_otros_paises","Nro_personas_otros_ingresos_instituciones","Nro_personas_otras_ganancias","Nro_personas_PET","Nro_personas_ocupadas","Nro_personas_desempleadas","Nro_personas_inactivas"))
+df_1 <- dvf_train %>% select(c("Nper","Pobre.1","tipo_vivienda.2","tipo_vivienda.3","tipo_vivienda.4","tipo_vivienda.5","Nro_cuartos", "Nro_personas_cuartos","arriendo","Nro_mujeres","edad_promedio","jefe_hogar_mujer","Nro_hijos","Nro_personas_trabajo_formal","edu_promedio","Nro_personas_subsidio_familiar","horas_trabajadas_promedio","Nro_personas_empleo_propio","Nro_personas_segundo_trabajo","Nro_personas_arriendos","Nro_personas_pensiones","Nro_personas_pension_alimenticia","Nro_personas_otros_ingresos","Nro_personas_otros_ingresos_pais","Nro_personas_otros_ingresos_otros_paises","Nro_personas_otros_ingresos_instituciones","Nro_personas_otras_ganancias","Nro_personas_PET","Nro_personas_ocupadas","Nro_personas_desempleadas","Nro_personas_inactivas"))
 
 ### Estadísticas descriptivas ###
 
