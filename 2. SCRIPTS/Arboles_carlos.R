6bbccdda9c4ea4b69d0b79ce4ef8d59168bcc141
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-02-26 11:30:24 -0500

    Se crea el script de arboles

---
+++ 2. SCRIPTS/Arboles_carlos.R
@@ -0,0 +1,14 @@
+#Problmem_Set_2
+# Arboles
+#Carlos Vergara, Alexandra Rizo, Danna Bolaños, Héctor Tacumán
+_______________________________________________________________
+
+#######Preparación del espacio########
+
+## Se llaman los paquetes para su uso en el Script:
+install.packages("pacman")
+require(pacman)
+p_load(tidyverse,rvest,writexl,rio,skimr,pastecs,PerformanceAnalytics,naniar,gtsummary,rpart,caret)
+
+set.seed(1011)
+
