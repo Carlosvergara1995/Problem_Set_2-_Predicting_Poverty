0a85bdecddf151fbe025d7ea63d802fcfe0653f9
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-02-25 22:54:53 -0500

    Se agrega y modifica el script

--- 2. SCRIPTS/Estad_descript.R
+++ 2. SCRIPTS/Estad_descript.R
@@ -17,7 +17,7 @@ dvf_train<- readRDS("dvf_train.rds")
 ####### Estadisticas descriptivas para la base de datos training ########
 
 dim(dvf_train)
-Pobres <- prop.table(table(dvf_train$Pobre.1))
+prop.table(table(dvf_train$Pobre.1))
 colnames(dvf_train)
 
 ## Se seleccionan variables de interes:
@@ -30,27 +30,20 @@ summary(df_1)
 
 # estadísticas descriptivas variables de interes
 
-require(gtsummary,flextable, officer) #llamado librería
-
-# estadísiticas descriptivas:
-
-stat.desc(df_1)
-descriptivas_trainig <- stat.desc(df_1)
-descriptivas_trainig$Estadisticas <- row.names(descriptivas_trainig) 
-descriptivas_trainig <- descriptivas_trainig %>% select(Estadisticas, everything())  
-write_xlsx(descriptivas_trainig, "descriptivas_trainig.xlsx")
-
-# Tablas descriptivas
-
 tbl_summary(df_1, statistic = list (all_continuous()~"{mean} ({sd})")) # generales
 tbl_summary(df_1, by= Pobre.1, statistic = list (all_continuous()~"{mean} ({sd})")) # por clasificación
 
-df_2<-tbl_summary(df_1, statistic = list (all_continuous()~"{mean} ({sd})")) # generales
+df_2 <-tbl_summary(df_1, statistic = list (all_continuous()~"{mean} ({sd})")) # generales
 df_3 <- tbl_summary(df_1, by= Pobre.1, statistic = list (all_continuous()~"{mean} ({sd})")) # por clasificación
 
-stargazer(nv_test_sinna, type="text", digits=5)
+#Se convierte la tabla por clasificación en formato word:
 
-df_1 %>% select(c("tipo_vivienda.3","tipo_vivienda.5","Nro_cuartos","Nro_mujeres","jefe_hogar_mujer","Nro_hijos","Nro_personas_trabajo_formal","edu_promedio", "Nro_personas_subsidio_familiar","horas_trabajadas_promedio","Nro_personas_segundo_trabajo","Nro_personas_pensiones","Nro_personas_pension_alimenticia","Nro_personas_otros_ingresos","Nro_personas_otros_ingresos_pais","Nro_personas_otros_ingresos_otros_paises","Nro_personas_otros_ingresos_instituciones","Nro_personas_otras_ganancias","Nro_personas_PET","Nro_personas_ocupadas","Nro_personas_desempleadas","Nro_personas_inactivas","Pobre.1")) %>% chart.Correlation()
+Tabla_est_desc_train <- as_tibble(df_3)
+tabla_flex <- flextable(Tabla_est_desc_train)
+doc <- read_docx()
+doc <- doc %>% 
+  body_add_flextable(tabla_flex)
+print(doc, target = "Tabla_est_desc_train.docx")
 
 ####### Estadisticas descriptivas para la base de datos testeo ########
 
