d8850c69505fde7b750bfcad6a66bbc1cb2b7d16
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-02-25 19:32:11 -0500

    Se cambio el script

--- 2. SCRIPTS/Estad_descript.R
+++ 2. SCRIPTS/Estad_descript.R
@@ -7,7 +7,7 @@
 ## Se llaman los paquetes para su uso en el Script:
 install.packages("pacman")
 require(pacman)
-p_load(tidyverse,rvest,writexl,rio,skimr,pastecs,PerformanceAnalytics,naniar,gtsummary)
+p_load(tidyverse,rvest,writexl,rio,skimr,pastecs,PerformanceAnalytics,naniar,gtsummary,flextable,officer)
 
 ## Se llaman las bases de datos: 
 
@@ -22,7 +22,7 @@ colnames(dvf_train)
 
 ## Se seleccionan variables de interes:
 
-df_1 <- dvf_train %>% select(c("Nper","Pobre.1","tipo_vivienda.2","tipo_vivienda.3","tipo_vivienda.4","tipo_vivienda.5","Nro_cuartos", "Nro_personas_cuartos","arriendo","Nro_mujeres","edad_promedio","jefe_hogar_mujer","Nro_hijos","Nro_personas_trabajo_formal","edu_promedio","Nro_personas_subsidio_familiar","horas_trabajadas_promedio","Nro_personas_empleo_propio","Nro_personas_segundo_trabajo","Nro_personas_arriendos","Nro_personas_pensiones","Nro_personas_pension_alimenticia","Nro_personas_otros_ingresos","Nro_personas_otros_ingresos_pais","Nro_personas_otros_ingresos_otros_paises","Nro_personas_otros_ingresos_instituciones","Nro_personas_otras_ganancias","Nro_personas_PET","Nro_personas_ocupadas","Nro_personas_desempleadas","Nro_personas_inactivas"))
+df_1 <- dvf_train %>% select(c("Pobre.1","Nper","tipo_vivienda.2","tipo_vivienda.3","tipo_vivienda.4","tipo_vivienda.5","Nro_cuartos", "Nro_personas_cuartos","arriendo","Nro_mujeres","edad_promedio","jefe_hogar_mujer","Nro_hijos","Nro_personas_trabajo_formal","edu_promedio","Nro_personas_subsidio_familiar","horas_trabajadas_promedio","Nro_personas_empleo_propio","Nro_personas_segundo_trabajo","Nro_personas_arriendos","Nro_personas_pensiones","Nro_personas_pension_alimenticia","Nro_personas_otros_ingresos","Nro_personas_otros_ingresos_pais","Nro_personas_otros_ingresos_otros_paises","Nro_personas_otros_ingresos_instituciones","Nro_personas_otras_ganancias","Nro_personas_PET","Nro_personas_ocupadas","Nro_personas_desempleadas","Nro_personas_inactivas"))
 
 ### Estadísticas descriptivas ###
 
@@ -30,9 +30,9 @@ summary(df_1)
 
 # estadísticas descriptivas variables de interes
 
-require(gtsummary) #llamado librería
+require(gtsummary,flextable, officer) #llamado librería
 
-# estadísiticas descriptivas generales datos:
+# estadísiticas descriptivas:
 
 stat.desc(df_1)
 descriptivas_trainig <- stat.desc(df_1)
@@ -43,3 +43,11 @@ write_xlsx(descriptivas_trainig, "descriptivas_trainig.xlsx")
 # Tablas descriptivas
 tbl_summary(df_1, statistic = list (all_continuous()~"{mean} ({sd})")) # generales
 tbl_summary(df_1, by= Pobre.1, statistic = list (all_continuous()~"{mean} ({sd})")) # por clasificación
+
+dvf_train %>% select(c("Pobre.1","Nper","tipo_vivienda.2","tipo_vivienda.3","tipo_vivienda.4","tipo_vivienda.5","Nro_cuartos", "Nro_personas_cuartos","arriendo","Nro_mujeres","edad_promedio","jefe_hogar_mujer","Nro_hijos","Nro_personas_trabajo_formal","edu_promedio","Nro_personas_subsidio_familiar","horas_trabajadas_promedio","Nro_personas_empleo_propio","Nro_personas_segundo_trabajo","Nro_personas_arriendos","Nro_personas_pensiones","Nro_personas_pension_alimenticia","Nro_personas_otros_ingresos","Nro_personas_otros_ingresos_pais","Nro_personas_otros_ingresos_otros_paises","Nro_personas_otros_ingresos_instituciones","Nro_personas_otras_ganancias","Nro_personas_PET","Nro_personas_ocupadas","Nro_personas_desempleadas","Nro_personas_inactivas"))%>% chart.Correlation()
+
+
+####### Estadisticas descriptivas para la base de datos testeo ########
+
+
+  
