d34ef96aa1d3915ec72228fa6d023773882794a1
Carlosvergara1995 <cd.vergara10@uniandes.edu.co>
2023-02-25 22:32:01 -0500

    Update Estad_descript.R

--- 2. SCRIPTS/Estad_descript.R
+++ 2. SCRIPTS/Estad_descript.R
@@ -7,7 +7,7 @@
 ## Se llaman los paquetes para su uso en el Script:
 install.packages("pacman")
 require(pacman)
-p_load(tidyverse,rvest,writexl,rio,skimr,pastecs,PerformanceAnalytics,naniar,gtsummary,flextable,officer)
+p_load(tidyverse,stargazer,rvest,writexl,rio,skimr,pastecs,PerformanceAnalytics,naniar,gtsummary,flextable,officer)
 
 ## Se llaman las bases de datos: 
 
@@ -41,29 +41,34 @@ descriptivas_trainig <- descriptivas_trainig %>% select(Estadisticas, everything
 write_xlsx(descriptivas_trainig, "descriptivas_trainig.xlsx")
 
 # Tablas descriptivas
+
 tbl_summary(df_1, statistic = list (all_continuous()~"{mean} ({sd})")) # generales
 tbl_summary(df_1, by= Pobre.1, statistic = list (all_continuous()~"{mean} ({sd})")) # por clasificación
 
-# Se profuden las tablas en Word: 
+df_2<-tbl_summary(df_1, statistic = list (all_continuous()~"{mean} ({sd})")) # generales
+df_3 <- tbl_summary(df_1, by= Pobre.1, statistic = list (all_continuous()~"{mean} ({sd})")) # por clasificación
 
-df_train_word <- df_1 %>% tbl_summary(by = tipo_vivienda.3, statistic = all_continuous() ~ "{mean} ({sd})")
+stargazer(nv_test_sinna, type="text", digits=5)
 
-# convertir la tabla a un objeto flextable
-tabla_flex <- flextable(df_1)
-
-# crear un objeto de documento Word
-doc <- read_docx()
-
-# agregar la tabla al documento
-doc <- doc %>% 
-  body_add_flextable(tabla_flex)
-
-# guardar el documento
-print(doc, target = "tabla_word.docx")
-
-df_1 %>% select(c("tipo_vivienda.3","tipo_vivienda.5","Nro_cuartos","Nro_mujeres","jefe_hogar_mujer","Nro_hijos","Nro_personas_trabajo_formal","edu_promedio", "Nro_personas_subsidio_familiar","horas_trabajadas_promedio","Nro_personas_segundo_trabajo","Nro_personas_pensiones","Nro_personas_pension_alimenticia","Nro_personas_otros_ingresos","Nro_personas_otros_ingresos_pais","Nro_personas_otros_ingresos_otros_paises","Nro_personas_otros_ingresos_instituciones","Nro_personas_otras_ganancias","Nro_personas_PET","Nro_personas_ocupadas","Nro_personas_desempleadas","Nro_personas_inactivas","Pobre.1"))
+df_1 %>% select(c("tipo_vivienda.3","tipo_vivienda.5","Nro_cuartos","Nro_mujeres","jefe_hogar_mujer","Nro_hijos","Nro_personas_trabajo_formal","edu_promedio", "Nro_personas_subsidio_familiar","horas_trabajadas_promedio","Nro_personas_segundo_trabajo","Nro_personas_pensiones","Nro_personas_pension_alimenticia","Nro_personas_otros_ingresos","Nro_personas_otros_ingresos_pais","Nro_personas_otros_ingresos_otros_paises","Nro_personas_otros_ingresos_instituciones","Nro_personas_otras_ganancias","Nro_personas_PET","Nro_personas_ocupadas","Nro_personas_desempleadas","Nro_personas_inactivas","Pobre.1")) %>% chart.Correlation()
 
 ####### Estadisticas descriptivas para la base de datos testeo ########
 
+dvf_test<- readRDS("dvf_test")
+dim(dvf_test)
+colnames(dvf_test)
+
+## Se seleccionan variables de interes
+df_4 <- dvf_test %>% select(c("tipo_vivienda.3","tipo_vivienda.5","Nro_cuartos","Nro_mujeres","jefe_hogar_mujer","Nro_hijos","Nro_personas_trabajo_formal","edu_promedio", "Nro_personas_subsidio_familiar","horas_trabajadas_promedio","Nro_personas_segundo_trabajo","Nro_personas_pensiones","Nro_personas_pension_alimenticia","Nro_personas_otros_ingresos","Nro_personas_otros_ingresos_pais","Nro_personas_otros_ingresos_otros_paises","Nro_personas_otros_ingresos_instituciones","Nro_personas_otras_ganancias","Nro_personas_PET","Nro_personas_ocupadas","Nro_personas_desempleadas","Nro_personas_inactivas","Pobre.1"))
+
+summary(df_4)
+
+# estadísticas descriptivas variables de interes
+
+require(gtsummary) #llamado librería
+
+# estadísiticas descriptivas generales datos
+
+tbl_summary(df_4, statistic = list (all_continuous()~"{mean} ({sd})")) # generales
 
   
